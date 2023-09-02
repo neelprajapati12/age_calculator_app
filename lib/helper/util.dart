bf80fde683a3c05a4702fedb4d93ee053272336e
neelprajapati12 <neelkprajapati12@gmail.com>
2023-09-02 14:35:54 +0530

    New Chnages

--- lib/helper/util.dart
+++ lib/helper/util.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-Widget vSize(double size) {
-  return SizedBox(height: size);
+Widget vSize(double size1) {
+  return SizedBox(height: size1);
 }
 
 Widget verticalDivider({double? height}) => Container(
       height: height ?? 30,
       width: 3,
-      color: Color.fromARGB(255, 112, 109, 109),
+      color: Color.fromARGB(255, 112, 109, 108),
     );
 
 Widget columnConatiner(BuildContext context,
